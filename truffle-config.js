bd9a3168aa21027d535ca445de10b308aab43a82
subin <github@subin.kr>
2023-05-14 16:47:23 +0900

    ganache deployed

--- truffle-config.js
+++ truffle-config.js
@@ -4,17 +4,11 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*", // Match any network id
-      gas: 6721975
     }
   },
-  // compilers: {
-  //   solc: {
-  //     settings: {
-  //       optimizer: {
-  //         enabled: true, // Default: false
-  //         runs: 200      // Default: 200
-  //       },
-  //     }
-  //   }
-  // }
+  compilers: {
+    solc: {
+      version: "0.8.15",
+    }
+  }
 };
